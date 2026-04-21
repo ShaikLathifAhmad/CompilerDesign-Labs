3585e8d13aa2363305deee85d0f107e0cd6b8966
Lathif <lathifahmad30@gmail.com>
2026-04-21 23:00:24 +0530

    Add NFA to DFA conversion function
    Implement NFA to DFA conversion algorithm.

---
+++ NFA → DFA.py
@@ -0,0 +1,21 @@
+def nfa_to_dfa(nfa):
+    dfa = {}
+    start = frozenset([0])
+    unmarked = [start]
+
+    while unmarked:
+        state = unmarked.pop()
+        dfa[state] = {}
+
+        for symbol in ['a', 'b']:
+            new_state = set()
+            for s in state:
+                if s in nfa and symbol in nfa[s]:
+                    new_state.update(nfa[s][symbol])
+            new_state = frozenset(new_state)
+
+            if new_state:
+                dfa[state][symbol] = new_state
+                if new_state not in dfa:
+                    unmarked.append(new_state)
+    return dfa

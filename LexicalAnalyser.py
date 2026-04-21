161dff30509c27033f179d3be52f281c934b030a
Lathif <lathifahmad30@gmail.com>
2026-04-21 22:59:08 +0530

    Implement lexical analyzer for code tokens

---
+++ LexicalAnalyser.py
@@ -0,0 +1,19 @@
+import re
+
+keywords = {"if", "else", "while", "int", "return"}
+
+def lexical_analyzer(code):
+    tokens = re.findall(r'[a-zA-Z_]\w*|\d+|[+\-*/=]', code)
+
+    for token in tokens:
+        if token in keywords:
+            print(token, "-> KEYWORD")
+        elif token.isdigit():
+            print(token, "-> NUMBER")
+        elif re.match(r'[a-zA-Z_]\w*', token):
+            print(token, "-> IDENTIFIER")
+        else:
+            print(token, "-> OPERATOR")
+
+code = "int a = 5 + b"
+lexical_analyzer(code)

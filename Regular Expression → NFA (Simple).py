53d5fb107b688a55dfd2753975caee4ebc686ed1
Lathif <lathifahmad30@gmail.com>
2026-04-21 22:59:53 +0530

    Add NFA class and regex_to_nfa function
    Implement a basic NFA class and regex to NFA conversion function.

---
+++ Regular Expression → NFA (Simple).py
@@ -0,0 +1,15 @@
+class NFA:
+    def __init__(self):
+        self.states = {}
+        self.start = 0
+        self.end = 1
+        self.states[0] = {}
+        self.states[1] = {}
+
+def regex_to_nfa(regex):
+    nfa = NFA()
+    nfa.states[0][regex] = [1]
+    return nfa
+
+nfa = regex_to_nfa('a')
+print(nfa.states)
